aa3665ccba8ab25d693f51e094853198db65399a
alanls1 <alanlima4123@gmail.com>
2025-08-07 12:42:28 -0300

    feat: first commit

---
+++ src/routes.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
+export const router = createBrowserRouter([
+  {
+    path: "",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
