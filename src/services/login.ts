73c854f38bfaa8ec34a798cefbd993154b0b8843
alanls1 <alanlima4123@gmail.com>
2025-08-08 17:43:43 -0300

    feat: create service to login

---
+++ src/services/login.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { api } from ".";
+import type { IValuesInput } from "../types/login";
+
+export const login = async ({ email, password }: IValuesInput) => {
+  try {
+    const res = await api.post("/users/login", {
+      email,
+      password,
+    });
+
+    if (res.data) {
+      const {
+        accessToken,
+        refreshToken,
+        user: { email, name, uid_usuarios },
+      } = res.data;
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+      localStorage.setItem("email", email);
+      localStorage.setItem("name", name);
+      localStorage.setItem("uid_usuarios", uid_usuarios);
+    }
+
+    return res.data;
+  } catch (error: any) {
+    throw error.response.data.message;
+  }
+};
