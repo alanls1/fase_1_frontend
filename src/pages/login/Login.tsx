9bcac177c9b50f69ce5e1d22f6628c3bf91b2248
alanls1 <alanlima4123@gmail.com>
2025-08-08 17:43:25 -0300

    feat: finished login page, now when logging redict to admin page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,22 +1,52 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Form from "../../components/form/Form";
 import "./Login.scss";
 import { Link } from "react-router-dom";
 import SectionLeft from "../../components/sectionLeft/SectionLeft";
+import { useMutation } from "@tanstack/react-query";
+import { login } from "../../services/login";
+import type { IValuesInput } from "../../types/login";
+import { useState } from "react";
+import Feedback from "../../components/feedback/Feedback";
 
 const Login = () => {
+  const [open, setOpen] = useState<{
+    isVisible: boolean;
+    message: string;
+  }>({ isVisible: false, message: "" });
+
+  const { mutate } = useMutation({
+    mutationFn: async (data: IValuesInput) => {
+      const res = await login(data);
+      return res;
+    },
+    onSuccess: () => {
+      window.location.href = "/admin/";
+    },
+    onError: (error: any) => {
+      console.log("error: ", error);
+      setOpen({ isVisible: true, message: error });
+    },
+  });
+
+  const values = (e: IValuesInput) => {
+    mutate(e);
+  };
+
   return (
     <div className="login-container">
       <div className="section-form">
         <SectionLeft />
         <div className="section-right">
-          <h3>Bem-vindo de volta!</h3>
-          <Form isLogin={true} labelButton="Entrar" />
+          <h2>Bem-vindo de volta!</h2>
+          <Form isLogin={true} labelButton="Entrar" handleSubmit={values} />
           <div>
             <p>Ainda não tem uma conta?</p>
             <Link to={"/register"}>Cadastre-se</Link>
           </div>
         </div>
       </div>
+      <Feedback open={open} setOpen={setOpen} />
     </div>
   );
 };
---
+++ src/types/login.ts
@@ -0,0 +1,13 @@
+export interface IValuesInput {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface IUser {
+  accessToken: string;
+  refreshToken: string;
+  email: string;
+  name: string;
+  uid_usuarios: string;
+}
