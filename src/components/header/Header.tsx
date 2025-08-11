8de76cd74ac23e75b67912e181351c30c0a937d6
alanls1 <alanlima4123@gmail.com>
2025-08-11 15:44:13 -0300

    feat: new functions to logout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,27 @@
+import { useMutation } from "@tanstack/react-query";
 import { useAuth } from "../../hook/useAuth";
 import Button from "../button/Button";
 import "./Header.scss";
+import { logout } from "../../services/login";
 
 const Header = () => {
   const { name } = useAuth();
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      await logout();
+    },
+    onSuccess: () => {
+      localStorage.clear();
+      window.location.href = "/";
+    },
+  });
 
+  const handleLogout = () => mutate();
   return (
     <header className="header-container">
       <nav>
         <h1>Olá, {name}</h1>
-        <Button type="button" variant="outlined">
+        <Button type="button" variant="outlined" onClick={handleLogout}>
           Sair
         </Button>
       </nav>
