5c6c4833f00609912fecbbe66d7bed5dbd37fdc9
alanls1 <alanlima4123@gmail.com>
2025-08-11 15:43:17 -0300

    feat: Footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import "./Footer.scss";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Fase 2</p>
+      <small>Desenvolvido por Alan.</small>
+    </div>
+  );
+};
